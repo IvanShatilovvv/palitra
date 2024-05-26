6b829bf8ee0648f09cc8d99e51b6df782c21c60d
ivan.shatilov.06@mail.ru <ivan.shatilov.06@mail.ru>
2024-05-27 00:37:14 +0500

    Добавьте файлы проекта.

---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            redSlider.ValueChanged += (s, e) => UpdateColor();
+            greenSlider.ValueChanged += (s, e) => UpdateColor();
+            blueSlider.ValueChanged += (s, e) => UpdateColor();
+        }
+
+        private void UpdateColor()
+        {
+            var color = Color.FromRgb((byte)redSlider.Value, (byte)greenSlider.Value, (byte)blueSlider.Value);
+            colorBrush.Color = color;
+        }
+    }
+}
